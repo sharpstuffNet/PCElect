3f6818b925f40ef0d80bbf90d9add27d3c9e8beb
Horst Richter <klorolle@hotmail.com>
2020-10-08 14:00:38 +0200

    more results

--- PCElect.Lib/PCElect.Results.cs
+++ PCElect.Lib/PCElect.Results.cs
@@ -29,8 +29,15 @@ namespace PCElect.Lib
 			var sb = new StringBuilder();
 			var firstVote = new Dictionary<string, OneVote>();
 			var lastVote = new Dictionary<string, OneVote>();
+			var perH = new Dictionary<int, int>();
+			DateTime? DTStart = null;
 
-			var VIDs = IO.File.ReadAllLines(IO.Path.Combine(_mainDir, "IDs.txt"));
+			var unknKey = 0;
+			var brknBC = 0;
+			var invalidVote = 0;
+			var mindchanged=0;
+
+			var VIDs = (from a in IO.File.ReadAllLines(IO.Path.Combine(_mainDir, "IDs.txt")) select HttpUtility.UrlDecode(a)).ToArray();
 			using (var md = SHA512.Create())
 			using (var csp = new RSACryptoServiceProvider(2048))
 			{
@@ -54,36 +61,74 @@ namespace PCElect.Lib
 						DT = DateTime.ParseExact(lines[1].Trim(), "yyyy MMdd HH:mm:ss.FFFF", CultureInfo.InvariantCulture),
 						Vote = lines[2].Trim().Split(',')
 					};
-					if (!VIDs.Contains(ov.Key) && !VIDs.Contains(HttpUtility.UrlEncode(ov.Key)))
+					if(VIDs[0] == ov.Key || VIDs[1] == ov.Key)
+                    {
+						if (!DTStart.HasValue)
+							DTStart = ov.DT;
+						continue;
+					}
+					if (!VIDs.Contains(ov.Key))
 					{
 						//Unknown Key
-
+						unknKey++;
 						continue;
 					}
 					else if (hsh != null && hsh != lines[3].Trim())
 					{
 						//Broken BC investigate
+						brknBC++;
 						Debugger.Break();
 					}
 					else if (ov.Vote.Length > 3)
 					{
 						//Ungültig
-
+						invalidVote++;
 						continue;
 					}
+					//Valid Vote
 					if (!firstVote.ContainsKey(ov.Key))
 						firstVote.Add(ov.Key, ov);
+					else
+						mindchanged++;
+
 					lastVote[ov.Key] = ov;
+
+					//Time
+					var hh = (int)((ov.DT - DTStart.Value).TotalHours);
+					if (!perH.ContainsKey(hh))
+						perH[hh] = 1;
+					else
+						perH[hh]++;
 
 					hsh = System.Convert.ToBase64String(md.ComputeHash(ba));
 				}
 			}
+			sb.AppendLine();
 
-			
+			sb.AppendLine("Stats");
+			sb.AppendLine($"Total Valid:\t{lastVote.Count}");
+			sb.AppendLine($"Unknown VID:\t{unknKey}");
+			sb.AppendLine($"Broken Chain:\t{brknBC}");
+			sb.AppendLine($"Invalid Vote:\t{invalidVote}");
+			sb.AppendLine($"Changed Vote:\t{mindchanged}");
+			sb.AppendLine($"Total Pos:\t{lastVote.Count*3}");
+			sb.AppendLine($"Total First:\t{(from a in firstVote.Values select a.Vote.Length).Sum()}");
+			sb.AppendLine($"Total Last:\t{(from a in lastVote.Values select a.Vote.Length).Sum()}");
+
+			sb.AppendLine("Times");
+            foreach (var item in from h in perH.OrderBy(x=>x.Key) select h)
+				sb.AppendLine($"{item.Key}\t{item.Value}");
+			sb.AppendLine();
+
+			sb.AppendLine("First Votes");
 			CreateStatistics(sb,firstVote.Values);
-			CreateStatistics(sb,lastVote.Values);
+			sb.AppendLine();
 
-			IO.File.WriteAllText(IO.Path.Combine(_mainDir, "Results.txt"),sb.ToString());
+			sb.AppendLine("Last Votes");
+			CreateStatistics(sb,lastVote.Values);
+			sb.AppendLine();
+
+			IO.File.WriteAllText(IO.Path.Combine(_mainDir, $"{DateTime.Now.ToString("yyMMdd HHmm")}_Results.txt"),sb.ToString());
 		}
 
 		private void CreateStatistics(StringBuilder sb, IEnumerable<OneVote> votes)
@@ -99,6 +144,9 @@ namespace PCElect.Lib
 						res[vv]++;
                 }
             }
+            foreach (var item in res.OrderBy(r=>r.Value))
+				sb.AppendLine($"{item.Key}\t{item.Value}");
+
 		}
 	}
 }
